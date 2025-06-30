415cf76a054d17e7af0f8d0de77342ba9600a803
Daniel <narselldev@gmail.com>
2025-06-30 00:49:03 -0500

    Improved clipboard rofi menu

--- scripts/scripts/cliphist-rofi-img.sh
+++ scripts/scripts/cliphist-rofi-img.sh
@@ -17,7 +17,7 @@ read -r -d '' prog <<EOF
 /^[0-9]+\s<meta http-equiv=/ { next }
 
 match(\$0, /^([0-9]+)\s(\[\[\s)?binary.*(jpg|jpeg|png|bmp)/, grp) {
-    system("echo " grp[1] "\\\\\t | cliphist decode >$tmp_dir/"grp[1]"."grp[3])
+    system("echo " grp[1] "\\\\\t -c | cliphist decode >$tmp_dir/"grp[1]"."grp[3])
     print substr(\$0, index(\$0, "\t") + 1) "\0info\x1f" \$0 "\x1ficon\x1f$tmp_dir/"grp[1]"."grp[3]
     next
 }
