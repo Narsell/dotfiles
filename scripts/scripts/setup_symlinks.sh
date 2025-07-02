7269805cfc55f84a4f56f4a038c3341d6d77af95
Daniel <narselldev@gmail.com>
2025-07-01 21:16:05 -0500

    Rofi app launcher

--- scripts/scripts/setup_symlinks.sh
+++ scripts/scripts/setup_symlinks.sh
@@ -3,4 +3,4 @@ mkdir -p ~/.config/rofi
 ln -s ~/.cache/wal/rofi-clipboard-config.rasi ~/.config/rofi/clipboard-config.rasi
 ln -s ~/.cache/wal/rofi-clipboard-delete-config.rasi ~/.config/rofi/clipboard-config-delete.rasi
 ln -s ~/.cache/wal/rofi-powermenu-config.rasi ~/.config/rofi/powermenu-config.rasi
-
+ln -s ~/.cache/wal/rofi-main-config.rasi ~/.config/rofi/config.rasi
