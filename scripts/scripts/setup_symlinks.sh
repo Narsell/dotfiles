54eab2399eb6374afeb87eb7b93a5ba632943315
Daniel <narselldev@gmail.com>
2026-02-10 21:07:14 -0500

    New pywal theme templates for yt music desktop, calibre and obsidian

--- scripts/scripts/setup_symlinks.sh
+++ scripts/scripts/setup_symlinks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Sets up the rofi config directory and symlinks the rasi config files from the generated wal rasi files in ~/.cache/wal/
+# Sets up the links from pywal's generated templates to their appropriate directories.
 
 config_dir="${HOME}/.config/rofi"
 
@@ -7,8 +7,15 @@ if [ ! -d "${config_dir}" ] ; then
 	mkdir -p "${config_dir}"
 fi
 
+# Rofi
 ln -s ~/.cache/wal/rofi-colors.rasi ~/.config/rofi/colors.rasi
 ln -s ~/.cache/wal/rofi-simple-config.rasi ~/.config/rofi/simple-config.rasi
 ln -s ~/.cache/wal/rofi-powermenu-config.rasi ~/.config/rofi/powermenu-config.rasi
 ln -s ~/.cache/wal/rofi-launcher-config.rasi ~/.config/rofi/launcher-config.rasi
 ln -s ~/.cache/wal/rofi-wallpaper-sel.rasi ~/.config/rofi/wallpaper-sel-config.rasi
+
+# Obsidian theme
+ln -s ~/.cache/wal/obsidian_template.css ~/Documents/Obsidian/Meditations/.obsidian/themes/Pywal/theme.css
+
+# Calibre theme
+ln -s ~/.cache/wal/pywal.calibre-palette ~/.config/calibre/palettes/pywal.calibre-palette
